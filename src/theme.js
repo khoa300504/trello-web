b06b20fceef3736448fbec79f9a78e5e05c00961
khoa300504 <khoa300504@gmail.com>
2024-02-10 16:03:36 +0700

    Dark&Light Mode #1

--- src/theme.js
+++ src/theme.js
@@ -1,23 +1,24 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: red[500]
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        // primary: {
+        //   main: '#ff5252'
+        // }
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    text: {
-      secondary: 'rgba(126, 255, 255, 0.4)'
-    },
-    error: {
-      main: red.A400
+    dark: {
+      palette: {
+        // primary: {
+        //   main: '#000'
+        // }
+      }
     }
   }
+  //...others properties
 })
 
 export default theme
