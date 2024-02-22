7da3b0c174f3cd194519fa7912da034016c0b250
khoa300504 <khoa300504@gmail.com>
2024-02-22 09:17:07 +0700

    Appbar #2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -29,7 +29,7 @@ function AppBar() {
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }}/>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-          <SvgIcon component={trelloLogo} inheritViewBox sx={{ color: 'primary.main' }} />
+          <SvgIcon fontSize='small' component={trelloLogo} inheritViewBox sx={{ color: 'primary.main' }} />
           <Typography variant='span' sx={{ color: 'primary.main', fontSize: '1.2rem', fontWeight: 'bold' }}>Trello</Typography>
         </Box>
         <Workspaces/>
@@ -43,11 +43,11 @@ function AppBar() {
         <ModeSelect/>
         <Tooltip title="Notification">
           <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
-            <NotificationsIcon/>
+            <NotificationsIcon sx={{ color: 'primary.main' }}/>
           </Badge>
         </Tooltip>
         <Tooltip title="Help">
-          <HelpOutlineOutlinedIcon sx={{ cursor: 'pointer' }}/>
+          <HelpOutlineOutlinedIcon sx={{ cursor: 'pointer', color: 'primary.main' }}/>
         </Tooltip>
         <Profile/>
       </Box>
--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,10 @@
+import { BorderAllOutlined } from '@mui/icons-material'
 import { blue, indigo, lightBlue, pink } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create a theme instance.
 const theme = extendTheme({
-  trello:{
+  trello: {
     appBarHeight: '58px',
     boardBarHeight: '60px'
   },
@@ -21,8 +22,48 @@ const theme = extendTheme({
         // }
       }
     }
+  },
+  components: {
+    // Name of the component
+    MuiButton: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem',
+          '.MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.light
+          },
+          '&:hover': {
+            '.MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.main
+            }
+          },
+          '& fieldset': {
+            borderWidth: '1px !important'
+          },
+          '& muiInp': {
+            borderWidth: '1px !important'
+          }
+        })
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    }
   }
-  //...others properties
 })
 
 export default theme
