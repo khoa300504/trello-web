a6d03137cd8ed3dd6e63a2866a3910e5507a3795
khoa300504 <khoa300504@gmail.com>
2024-04-13 09:48:22 +0700

    Build_Mode dev and pro

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,41 +2,41 @@ import { API_ROOT } from '~/utils/constants'
 import axios from 'axios'
 
 export const fetchBoardDetailsAPI = async (boardId) => {
-  const response = await axios.get(`${API_ROOT}v1/boards/${boardId}`)
+  const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
   //Axios trả về kq về property của nó là data
   return response.data
 }
 
 export const updateBoardDetailsAPI = async (boardId, updateData) => {
-  const response = await axios.put(`${API_ROOT}v1/boards/${boardId}`, updateData)
+  const response = await axios.put(`${API_ROOT}/v1/boards/${boardId}`, updateData)
   //Axios trả về kq về property của nó là data
   return response.data
 }
 
 export const moveCardToDifferentColumnAPI = async (updateData) => {
-  const response = await axios.put(`${API_ROOT}v1/boards/supports/moving_card`, updateData)
+  const response = await axios.put(`${API_ROOT}/v1/boards/supports/moving_card`, updateData)
   //Axios trả về kq về property của nó là data
   return response.data
 }
 
 export const createNewColumnAPI = async (newColumnData) => {
-  const response = await axios.post(`${API_ROOT}v1/columns`, newColumnData)
+  const response = await axios.post(`${API_ROOT}/v1/columns`, newColumnData)
   return response.data
 }
 
 export const updateColumnDetailsAPI = async (columnId, updateData) => {
-  const response = await axios.put(`${API_ROOT}v1/columns/${columnId}`, updateData)
+  const response = await axios.put(`${API_ROOT}/v1/columns/${columnId}`, updateData)
   //Axios trả về kq về property của nó là data
   return response.data
 }
 
 export const deleteColumnDetailsAPI = async (columnId) => {
-  const response = await axios.delete(`${API_ROOT}v1/columns/${columnId}`)
+  const response = await axios.delete(`${API_ROOT}/v1/columns/${columnId}`)
   //Axios trả về kq về property của nó là data
   return response.data
 }
 
 export const createNewCardAPI = async (newCardData) => {
-  const response = await axios.post(`${API_ROOT}v1/cards`, newCardData)
+  const response = await axios.post(`${API_ROOT}/v1/cards`, newCardData)
   return response.data
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,15 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-xsa1.onrender.com/'
+let apiRoot = ''
+console.log('🚀 ~ import.meta.env:', import.meta.env)
+console.log('🚀 ~ process.env:', process.env)
+
+if (process.env.BUILD_MODE === 'dev')
+{
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production')
+{
+  apiRoot = 'https://trello-api-xsa1.onrender.com'
+}
+console.log('🚀 ~ apiRoot:', apiRoot)
+
+export const API_ROOT = apiRoot
