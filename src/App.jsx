8efd5372bcd6ba6ee9610dd7a2bfa3fe87dde1d0
khoa300504 <khoa300504@gmail.com>
2024-10-30 21:16:12 +0700

    Add Auth Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Board from '~/pages/Boards/_id'
 import NotFound from './pages/404/NotFound'
+import Auth from './pages/Auth/Auth'
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
       } />
       {/* Board Details */}
       <Route path='/boards/:boardId' element={<Board/>} />
+
+      {/* Authentication */}
+      <Route path='/login' element={<Auth/>} />
+      <Route path='/register' element={<Auth/>} />
+
       {/* 404 Page */}
       <Route path='*' element={<NotFound/>} />
     </Routes>
